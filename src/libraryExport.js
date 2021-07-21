8aa63f02fd3ba69f1bff7ac106a7a86748633478
Kevin Ghadyani <Saturn2888@gmail.com>
2021-07-21 03:57:34 -0500

    🚑️ Added missing export for `convertToReactComponent`

--- src/libraryExport.js
+++ src/libraryExport.js
@@ -1,2 +1,3 @@
+export { default as convertToReactComponent } from './react/convertToReactComponent.jsx'
 export { default as ReactToSolidBridge } from './react/ReactToSolidBridge.jsx'
 export { default as ReactToSolidBridgeProvider } from './react/ReactToSolidBridgeProvider.jsx'
